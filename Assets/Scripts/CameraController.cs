b4175bb28717cea75ae1da5fdc0c912d34745106
Shinsuke Ishimaru <shinsuke.ishimaru@gmail.com>
2020-10-09 16:06:41 +0900

    update

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, Time.deltaTime * 10f, 0f), Space.Self);
+    }
+}
